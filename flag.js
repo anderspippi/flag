ebbd848d1e7f8e9af54fc643b4211f17c1538070
Emil Brink <emil@obsession.se>
2014-12-02 10:55:56 +0100

    Blit 1-pixel wide columns rather than single pixels. Much faster.

--- flag.js
+++ flag.js
@@ -20,7 +20,6 @@ var Flag = function(canvas, text) {
 	this.textcontext.fillText(text, this.textbuf.width / 2, this.textbuf.height / 2, this.textbuf.width);
 
 	this.dw = this.canvas.width / this.textbuf.width;
-	this.dh = this.canvas.height / this.textbuf.height;
 
 	this.radius = (this.canvas.height - this.textcontext.canvas.height) / 4;
 
@@ -30,19 +29,18 @@ var Flag = function(canvas, text) {
 		var src = this.textcontext;
 		var dst = this.context;
 		var angle = this.angle;
+		var cy = (dst.canvas.height - src.canvas.height) / 2;
 		dst.clearRect(0, 0, dst.canvas.width, dst.canvas.height);
 		for(var x = 0; x < src.canvas.width; ++x) {
-			for(var y = 0; y < src.canvas.height; ++y) {
-				var dx = x * this.dw;
-				var dy = y * this.dh + this.radius * Math.sin(angle) * Math.cos(angle / 3);
-				dst.drawImage(src.canvas, x, y, 1, 1, dx, dy, this.dw, this.dh);
-			}
+			var dx = x * this.dw;
+			var dy = this.radius * Math.sin(angle) * Math.cos(angle / 3) * Math.sin(angle / 5);
+			dst.drawImage(src.canvas, x, 0, 1, src.canvas.height, dx, dy, this.dw, dst.canvas.height);
 			angle += 0.2;
 		}
 	};
 };
 
-// Find all #flag-classed canvases in the page.
+// Find all flag-classed canvases in the page.
 var canvases = document.body.getElementsByTagName("canvas");
 for(var i = 0; i < canvases.length; ++i) {
 	if(canvases[i].className == "flag") {
